8d03d617ccc4b7c66d95ff61acc479f0fd46e8d6
Thejustv <thejustv22@gmail.com>
2024-01-25 22:52:30 +0530

    added splash screen

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { View,Text, SafeAreaView } from "react-native";
+import React,{useEffect} from "react";
+import { Text, SafeAreaView } from "react-native";
+import BootSplash from "react-native-bootsplash";
 
 const App=()=>{
+
+    useEffect(()=>{
+        const init = async () => {
+            // …do multiple sync or async tasks
+          };
+      
+          init().finally(async () => {
+            await BootSplash.hide({ fade: true });
+          });
+    },[])
+
     return (
-        <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'black'}}>
+        <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
             <Text style={{color:'red'}}>Hello World!</Text>
         </SafeAreaView>
     )
